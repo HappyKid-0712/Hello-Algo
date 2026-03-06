e6cadcd5c6ae27439208aecd733ac8245af8f5b4
Flames4Yan <1397610285@qq.com>
2026-03-06 14:04:09 +0800

    新增 Windows 环境下的 C++ 配置文件，更新 .gitignore，添加 test.cpp 示例代码

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+const int MAXN = 1e5 + 5;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0), cout.tie(0);
+  int iok = 0;
+  cout << "hellworld";
+  return 0;
+}
