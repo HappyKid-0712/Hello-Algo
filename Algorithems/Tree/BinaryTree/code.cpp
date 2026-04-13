4bf73871722c1223a455a423c40f1727a297a18f
6ICKO6OY <1397610285@qq.com>
2026-04-14 07:39:22 +0800

    Refactor code structure for improved readability and maintainability

--- Algorithems/Tree/BinaryTree/code.cpp
+++ Algorithems/Tree/BinaryTree/code.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MAXN = 1e5 + 5;
+/*
+对于二叉树来说，一般就是用一个vec去接纳节点，所有创建的节点都该被传入进去。
+然后对于节点的组织，其实交给结构体的定义就行了，因为结构体的lchild和rchild已经定义了结构之间的关系了。
+这种思想对于很多节点类的数据结构都适用
+删除节点不能用erase，因为节点会自动往前走，下标和变化，后面的节点就全乱了，那么删除节点就断开链接就行了。
+可以开个stack回收池子去回收节点，然后修改newNodeForVec的算法。
+*/
 struct node
 {
   char val;
@@ -25,6 +32,7 @@ int newNodeForStatic(char val)
   binTeeStatic[idx] = node(val);
   return idx++;
 }
+
 void insertNode(int fatherIndex, int childeIndex, bool LorR)
 {
   if (LorR)
@@ -35,17 +43,18 @@ void insertNode(int fatherIndex, int childeIndex, bool LorR)
     binTreeVec[fatherIndex].rchild = childeIndex;
 }
 
-int createNewTree(deque<char> &valQue)
+int createNewTree(deque<char> &valQue) // 传入一个要处理的数值队列
 {
   int root = newNodeForVec(valQue.front());
-  valQue.pop_front();
-  queue<int> qu;
+  valQue.pop_front(); // 取头节点出来
+
+  queue<int> qu; // 遍历用的队列
   qu.push(root);
   while (!qu.empty())
   {
     int curr = qu.front();
     qu.pop();
-    if (!valQue.empty() && valQue.front() != '0')
+    if (!valQue.empty() && valQue.front() != '0') // 零代表是空的，没有这个数值
     {
       int lidx = newNodeForVec(valQue.front()); // 分开写，写在一起会爆炸
       binTreeVec[curr].lchild = lidx;
@@ -107,8 +116,8 @@ int main()
 {
   deque<char> dque = {'E', 'B', 'G', 'A', 'D', 'F', 'I', '0', '0', 'C', '0', '0', 'H', '0'};
   int root = createNewTree(dque);
-  printPreTree(root);
+  printPreTree(root); // 前序遍历
   cout << endl;
-  printQueTree(root);
+  printQueTree(root); // BFS
   cout << endl;
 }
